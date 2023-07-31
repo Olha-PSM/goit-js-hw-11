41407cca6071d44b08a37bc9745cc0cc182a8573
Olha-PSM <Olha84pismennaa@gmail.com>
2023-07-31 07:54:48 +0300

    tasck 11

--- src/index.js
+++ src/index.js
@@ -1,97 +1,128 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38516408-b596b12dec0c344cdc294ce99';
+const axios = require('axios').default;
+
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import NewsApiService from './js/api-serves';
 
-let totalHits = 0;
-let page = 1;
-
-const elements = {
-  searchForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-};
-console.log(elements);
-
-const { searchForm, loadMoreBtn, gallery } = elements;
-
-const newsApiService = new NewsApiService();
-console.log(newsApiService);
-
+const form = document.getElementById('search-form');
+const input = document.querySelector('.js-input');
+const gallery = document.querySelector('.js-gallery');
+const loadBtn = document.querySelector('.load-js');
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: `alt`,
   captionDelay: 250,
 });
-loadMoreBtn.style.display = 'none';
-searchForm.addEventListener('submit', onSubmit);
-loadMoreBtn.addEventListener('click', onLoadMore);
+let page = 1;
+let perPage = 40;
+let totalHits = 0;
+let searchQuery = '';
 
-async function onSubmit(e) {
+loadBtn.style.display = 'none';
+
+form.addEventListener('submit', handleSubmit);
+loadBtn.addEventListener('click', handleLoadMore);
+
+async function handleSubmit(e) {
   e.preventDefault();
   page = 1;
-  newsApiService.resetPage();
-  clearGalleryContainer();
-  newsApiService.query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadBtn.style.display = 'none';
+  const value = input.value;
+  try {
+    const data = await fetchImages(value);
 
-  if (newsApiService.query === '') {
-    Notiflix.Notify.warning('Please, fill the main field');
-    return;
+    if (data.hits.length === 0) {
+      showError();
+    } else {
+      showImages(data.hits);
+      totalHits = data.totalHits;
+
+      if (page * perPage < totalHits) {
+        loadBtn.style.display = 'block';
+      } else {
+        showEndMessage();
+      }
+    }
+
+    page += 1;
+  } catch (error) {
+    console.log(error);
+    showError();
   }
-
-  fetchGallery();
 }
 
-function fetchGallery() {
-  loadMoreBtn.style.display = 'block';
-  newsApiService.fetchArticls().then(showImages);
+async function handleLoadMore() {
+  try {
+    const value = input.value;
+    const data = await fetchImages(value);
+
+    if (data.hits.length === 0) {
+      showError();
+    } else {
+      showImages(data.hits);
+      totalHits = data.totalHits;
+
+      if (page * perPage < totalHits) {
+        loadBtn.style.display = 'block';
+      } else {
+        showEndMessage();
+      }
+    }
+
+    page += 1;
+  } catch (error) {
+    console.log(error);
+    showError();
+  }
 }
 
-function onLoadMore() {
-  newsApiService.fetchArticls().then(showImages);
+async function fetchImages(value) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  const { data } = await axios.get(url);
+  return data;
 }
 
 function showImages(images) {
   const markup = images
     .map(
       ({
-        tags,
         largeImageURL,
+        tags,
         webformatURL,
         likes,
         views,
         comments,
         downloads,
       }) => `
-      <div class="photo-card">
-      <a  class="photo-link" href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-img"
-      />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes ${likes}</b>
-      </p>
-      <p class="info-item">
-        <b>Views ${views}</b>
-      </p>
-      <p class="info-item">
-        <b>Comments ${comments}</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads ${downloads}</b>
-      </p>
-    </div>
+    <div class="photo-card">
+    <a  class="photo-link" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
   </div>
-      </div>
-
-    `
+</div>
+    </div>
+  `
     )
     .join('');
+
   gallery.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
 }
-function clearGalleryContainer() {
-  gallery.innerHTML = '';
-}
+
 function showError() {
   Notiflix.Notify.failure(
     'Sorry, there are no images matching your search query. Please try again.'
@@ -102,5 +133,5 @@ function showEndMessage() {
   Notiflix.Notify.info(
     "We're sorry, but you've reached the end of search results."
   );
-  loadMoreBtn.style.display = 'none'; // Hide the button when reaching the end of results
+  loadBtn.style.display = 'none';
 }
